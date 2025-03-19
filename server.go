f5ebb56c018fe0de26ecde2780a9f159a5dba372
David Alecrim <dsalecrim@outlook.com>
2025-03-19 11:58:26 -0300

    feat: add bad requests handling

--- server.go
+++ server.go
@@ -15,7 +15,7 @@ type Server struct {
 	ln   net.Listener
 }
 
-// addr -> ":8080"
+// e.g. addr -> ":8080"
 func NewServer(addr string) *Server {
 	return &Server{
 		addr: addr,
@@ -56,12 +56,7 @@ func (s *Server) handleConnection(conn net.Conn) {
 	go s.readConnection(conn, readCallback)
 
 	for request := range readCallback {
-		response, err := s.handleRequest(request)
-		if err != nil {
-			slog.Error("failed to handle request", "error", err)
-			continue
-		}
-
+		response := s.handleRequest(request)
 		_, err = conn.Write(response)
 		if err != nil {
 			slog.Debug("failed to write to the connection")
@@ -74,11 +69,17 @@ func (s *Server) handleConnection(conn net.Conn) {
 	}
 }
 
-func (s *Server) handleRequest(request []byte) (response []byte, err error) {
-	path := strings.Split(string(request), " ")[1]
+func (s *Server) handleRequest(request []byte) (response []byte) {
+	defer func() {
+		if r := recover(); r != nil {
+			slog.Debug("recovered from panic", "recover", r)
+			response = []byte("HTTP/1.1 400 Bad Request\r\n\r\n")
+		}
+	}()
 
+	path := strings.Split(string(request), " ")[1]
 	if path == "/" {
-		return []byte("HTTP/1.1 200 OK\r\n\r\n"), nil
+		return []byte("HTTP/1.1 200 OK\r\n\r\n")
 	}
 
 	if strings.HasPrefix(path, "/echo/") {
@@ -86,13 +87,13 @@ func (s *Server) handleRequest(request []byte) (response []byte, err error) {
 		requestUrlPath := string(parsedRequest[1]) // after the GET
 		afterEcho := strings.Split(requestUrlPath, "/")[2]
 
-		response := []byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\n")
+		response = []byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\n")
 		response = append(response, []byte(afterEcho)...)
 
-		return response, nil
+		return response
 	}
 
-	return []byte("HTTP/1.1 404 Not Found\r\n\r\n"), nil
+	return []byte("HTTP/1.1 404 Not Found\r\n\r\n")
 }
 
 func (s *Server) readConnection(conn net.Conn, readCallback chan []byte) {
--- test/e2e_test.go
+++ test/e2e_test.go
@@ -86,4 +86,23 @@ func TestE2E(t *testing.T) {
 
 		assert.Equal(t, readBuf[:n], []byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\nabc"))
 	})
+
+	t.Run("should return 400 to a bad request", func(t *testing.T) {
+		conn, err := net.Dial("tcp", ":8097")
+		require.NoError(t, err)
+		require.NoError(t, conn.SetDeadline(time.Now().Add(time.Second*5)))
+
+		writeBuf := []byte(
+			"INVALID",
+		)
+		n, err := conn.Write(writeBuf)
+		require.NoError(t, err)
+		assert.Greater(t, n, 0)
+
+		readBuf := make([]byte, 1024)
+		n, err = conn.Read(readBuf)
+		require.NoError(t, err)
+
+		assert.Equal(t, readBuf[:n], []byte("HTTP/1.1 400 Bad Request\r\n\r\n"))
+	})
 }
